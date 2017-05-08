188492e196246ea2538d9a06bc31e1a89006bb3d
Eduardo Polmann <dudupolmann@gmail.com>
2017-05-08 09:41:36 -0300

    [189312] Use boostrap

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
@@ -39,6 +42,6 @@ io.on('connection', function(socket) {
 
 });
 
-http.listen(3000, function(){
+http.listen(3000, function() {
   console.log('listening on *:3000');
 });
