b5a10e8c98119647d9c3e7744a8b7d0e396bfd4b
Eduardo Polmann <dudupolmann@gmail.com>
2017-05-12 15:25:03 -0300

    Added Babel, Webpack and React. Also reformatted code.

---
+++ app/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Navbar extends React.Component {
+
+    render() {
+        return (
+            <nav className="navbar navbar-default">
+                <a className="navbar-brand col-sm-4 col-md-3" href="/">WebChat</a>
+            </nav>
+        );
+    }
+
+}
--- index.js
+++ index.js
@@ -7,58 +7,58 @@ var io = require('socket.io')(http);
 
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-  res.redirect('/login');
+app.get('/', function (req, res) {
+    res.redirect('/login');
 });
 
-app.post('/', function(req, res) {
-  console.log(req.body);
-  console.log(req.cookies);
-  res.cookie('username', req.body.username);
-  res.sendFile(__dirname + '/index.html');
+app.post('/', function (req, res) {
+    console.log(req.body);
+    console.log(req.cookies);
+    res.cookie('username', req.body.username);
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/login', function(req, res) {
-  res.sendFile(__dirname + '/login.html');
+app.get('/login', function (req, res) {
+    res.sendFile(__dirname + '/login.html');
 });
 
 
 var connectedUsers = {};
 
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
 
-  socket.on('hello', function(user) {
-    socket.emit('usersonline', connectedUsers);
-    connectedUsers[socket.id] = user;
-    socket.broadcast.emit('userconnected', user);
-    socket.broadcast.emit('chat', user + ' is connected');
-  });
+    socket.on('hello', function (user) {
+        socket.emit('usersonline', connectedUsers);
+        connectedUsers[socket.id] = user;
+        socket.broadcast.emit('userconnected', user);
+        socket.broadcast.emit('chat', user + ' is connected');
+    });
 
-  socket.on('typing', function() {
-    var user = connectedUsers[socket.id];
-    user = (user) ? user : 'anonymous';
-    socket.broadcast.emit('typing', user);
-  });
+    socket.on('typing', function () {
+        var user = connectedUsers[socket.id];
+        user = (user) ? user : 'anonymous';
+        socket.broadcast.emit('typing', user);
+    });
 
-  socket.on('chat', function(msg) {
-    var user = connectedUsers[socket.id];
-    user = (user) ? user : 'anonymous';
-    socket.broadcast.emit('chat', user +' - '+ msg);
-  });
+    socket.on('chat', function (msg) {
+        var user = connectedUsers[socket.id];
+        user = (user) ? user : 'anonymous';
+        socket.broadcast.emit('chat', user + ' - ' + msg);
+    });
 
-  socket.on('disconnect', function() {
-    var user = connectedUsers[socket.id];
-    socket.broadcast.emit('userdisconnected', user);
-    socket.broadcast.emit('chat', 'user ' + user + ' disconnected');
-    delete user;
-  });
+    socket.on('disconnect', function () {
+        var user = connectedUsers[socket.id];
+        socket.broadcast.emit('userdisconnected', user);
+        socket.broadcast.emit('chat', 'user ' + user + ' disconnected');
+        delete user;
+    });
 
 });
 
-http.listen(3000, function() {
-  console.log('listening on *:3000');
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
